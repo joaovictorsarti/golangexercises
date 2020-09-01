b784137857759209233b7d063969d78c04b9e966
João Victor Sarti <joaovictorsarti@hotmail.com>
2020-09-01 18:26:43 -0300

    Adicionando callbacks e closure

---
+++ funcoes/closure.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+//closure - captura de scope
+
+func main() {
+
+	a := i()
+
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(a())
+
+	b := i()
+
+	fmt.Println(b())
+	fmt.Println(b())
+	fmt.Println(b())
+}
+
+func i() func() int {
+	x := 0
+	return func() int {
+		x++
+		return x
+	}
+}
